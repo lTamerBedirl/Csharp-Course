8f54597c468b69ea472daff702635a20edf7dc9f
Tamer Bedir <tamerbedir_81@hotmail.com>
2018-01-13 01:10:50 +0300

    Fist Csharp program
    This is my first Csharp Program

---
+++ Myvriables/Myvriables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myvriables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myvalue = 7;
+            Console.Write("{0}", myvalue);
+            Console.ReadKey();
+        }
+    }
+}
